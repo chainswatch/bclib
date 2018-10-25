caed50f26e062a34c7b267cafa01f7c2baf18643
Kevin Primicerio <admin@example.com>
2018-10-25 12:13:56 +0200

    Feature/coverage

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build Status](https://git.posc.in/cw/watchers/badges/master/build.svg)](https://git.posc.in/cw/watchers/commits/master)
+[![Coverage Report](https://git.posc.in/cw/watchers/badges/master/coverage.svg)](https://git.posc.in/cw/watchers/commits/master)
+[![Go Report Card](https://goreportcard.com/badge/git.posc.in/cw/watchers)](https://goreportcard.com/report/git.posc.in/cw/watchers)
---
+++ tools/coverage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+COVERAGE_DIR="${COVERAGE_DIR:-coverage}"
+PKG_LIST=$(go list ./... | grep -v /vendor/ | tr -d '\r')
+
+# Create the coverage files directory
+mkdir -p "$COVERAGE_DIR"
+
+echo 'mode: count' > "${COVERAGE_DIR}"/coverage.cov
+# Create a coverage file for each package
+for package in ${PKG_LIST}; do
+    go test -covermode=count -coverprofile "${COVERAGE_DIR}/${package##*/}.cov" "$package"
+    tail -q -n +2 "${COVERAGE_DIR}/${package##*/}.cov" >> "${COVERAGE_DIR}"/coverage.cov
+done
+
+# Display the global code coverage
+go tool cover -func="${COVERAGE_DIR}"/coverage.cov
+
+# If needed, generate HTML report
+if [ "$1" == "html" ]; then
+    go tool cover -html="${COVERAGE_DIR}"/coverage.cov -o coverage.html
+fi
+
+# Remove the coverage files directory
+rm -rf "$COVERAGE_DIR"
