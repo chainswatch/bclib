078ef163294b4412f2c6baafa13b3f9a2dde32e7
Kevin Primicerio <kevin.primicerio@gmail.com>
2019-03-06 17:22:01 +0100

    parser tests

---
+++ parser/parser_test.go
@@ -0,0 +1,34 @@
+package parser
+
+import (
+	"testing"
+	"github.com/chainswatch/bclib/serial"
+)
+
+func TestParser(t *testing.T) {
+
+	for i := uint64(0); i < 10000; i++ {
+		b := CompactSize(i)
+		raw, err := New(b)
+		if err != nil {
+			t.Fatal(err)
+		}
+		j := raw.ReadCompactSize()
+		if i != j {
+			t.Fatalf("%d != %d", i, j)
+		}
+	}
+
+
+	raw, err := serial.HexToBinary([]byte("fde814"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	br, err := New(raw)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if br.ReadCompactSize() != 5352 {
+		t.Errorf("!= 5352")
+	}
+}
