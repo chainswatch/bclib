1e0573c088c385a2d9f10dffa20bc9c5bce04a14
Kevin Primicerio <kevin.primicerio@gmail.com>
2019-07-09 10:28:41 +0200

    add block header tests

--- btc/block_test.go
+++ btc/block_test.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"bytes"
 	"strconv"
 	"testing"
 )
@@ -17,37 +18,52 @@ func TestBlock(t *testing.T) {
 		name    string
 		fileNum int
 		height  uint32
+		hash		[]byte
 	}{
 		{
-			name:    "Random block 1",
+			name:    "Block 547066",
 			fileNum: 1,
 			height:  547066,
+			hash:   []byte("000000000000000000187868fe3ac0d0bbd36161f0d97e3332928f2ea54baf5a"),
 		},
 		{
 			name:    "Block 251718",
 			fileNum: 251718,
 			height:  251718,
+			hash:   []byte("000000000000001fbc5a74fb56b1cf8949bcfad8e3ae06f2af638b94f7633fbc"),
 		},
 		{
 			name:    "Block 265458",
 			fileNum: 265458,
 			height:  265458,
+			hash:   []byte("000000000000000b7e48f88e86ceee3e97b4df7c139f5411d14735c1b3c36791"),
 		},
 	}
 
 	for _, test := range tests {
 		raw, err := ioutil.ReadFile("./testdata/rawblock_" + strconv.Itoa(test.fileNum))
 		if err != nil {
-			t.Error(err)
+			t.Fatalf("%s: %s", test.name, err)
 		}
 		rawBlock, err := parser.New(raw)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("%s: %s", test.name, err)
 		}
 		b, err := DecodeBlock(rawBlock)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("%s: %s", test.name, err)
 		}
+
+		// Test block header
+		thash, err := serial.HexToBinary(test.hash)
+		if err != nil {
+			t.Fatalf("%s: %s", test.name, err)
+		}
+		bhash := serial.ReverseHex(b.Hash)
+		if bytes.Compare(bhash, thash) != 0 {
+			t.Errorf("%s: Wrong block header: %x != %x", test.name, bhash, thash)
+		}
+
 		if b.NTx != uint32(len(b.Txs)) {
 			t.Error("Wrong number of transactions: ", b.NTx, " != ", len(b.Txs))
 		}
